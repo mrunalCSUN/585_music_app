3a41160bd6bd122823e44d4234be78d9145e54d4
vi1801 <81208569+vi1801@users.noreply.github.com>
2022-11-09 16:37:54 -0800

    Update Seekbar.jsx

--- Seekbar.jsx
+++ Seekbar.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Seekbar = ({ value, min, max, onInput, setSeekTime, appTime }) => {
   // converts the time to format 0:00
-  const getTime = (time) => `${Math.floor(time / 60)}:${(`0${Math.floor(time % 60)}`).slice(-2)}`;
+  // Math.floor() function always rounds down and returns the largest integer less than or equal to a given number
+  const getTime = (time) => `${Math.floor(time / 60)}:${(`0${Math.floor(time % 60)}`).slice(-2)}`;  //.slice() gives portion from start to end
 
   return (
     <div className="hidden sm:flex flex-row items-center">
